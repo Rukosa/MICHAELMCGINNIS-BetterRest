8c2b7f08da2e47674583950db264bb8508757c44
Rukosa <mwmcginnis490@gmail.com>
2022-02-14 14:45:14 -0800

    Initial Commit

---
+++ MICHAELMCGINNIS-BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MICHAELMCGINNIS-BetterRest
+//
+//  Created by Michael Mcginnis on 2/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MICHAELMCGINNIS-BetterRest/MICHAELMCGINNIS_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_BetterRestApp.swift
+//  MICHAELMCGINNIS-BetterRest
+//
+//  Created by Michael Mcginnis on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
